96706c1e29aca6ccb419d47a46a181c8c9b09e42
WHITE-GRINT\Kazuki <kzktcy0610@gmail.com>
2020-12-03 21:58:52 +0900

    Controllerをスキャンさせるために追加

--- chat2/src/main/java/com/example/application/Chat2Application.java
+++ chat2/src/main/java/com/example/application/Chat2Application.java
@@ -2,7 +2,9 @@ package com.example.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = "com.example")
 @SpringBootApplication
 public class Chat2Application {
 
