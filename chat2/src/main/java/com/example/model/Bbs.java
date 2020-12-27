49fc723cfd08f3790c87f34e485074731da65725
WHITE-GRINT\Kazuki <kzktcy0610@gmail.com>
2020-12-27 21:51:42 +0900

    モデルの作成

---
+++ chat2/src/main/java/com/example/model/Bbs.java
@@ -0,0 +1,13 @@
+package com.example.model;
+
+import lombok.Data;
+
+@Data
+public class Bbs {
+
+	private int id;
+	private String author;
+	private String subject;
+	private String body;
+	private int deleteFlag;
+}
