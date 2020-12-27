2be6434e1a4c749a55f17564d143ea6cac726ce7
WHITE-GRINT\Kazuki <kzktcy0610@gmail.com>
2020-12-27 21:33:57 +0900

    Iwasaki用SQL修正

--- chat2/src/main/resources/data.sql
+++ chat2/src/main/resources/data.sql
@@ -1,2 +1,14 @@
-INSERT INTO employee (employee_id, employee_name, age)
-VALUES(1, '山田太郎', 30);
+INSERT INTO user (id ,name, password)
+VALUES(1, 'Iwasaki', 'hoge');
+
+INSERT INTO user (id, name, password)
+VALUES(2, 'Ochi', 'fuga');
+
+INSERT INTO bbs (id ,author, subject, body, delete_flg)
+VALUES(1, 'Iwasaki', 'first', 'こんにちはー。', 0);
+
+INSERT INTO bbs (id ,author, subject, body, delete_flg)
+VALUES(2, 'Ochi', 'secound', 'こんにちはー。岩崎さん', 0);
+
+INSERT INTO bbs (id ,author, subject, body, delete_flg)
+VALUES(3, 'Ochi', 'third', 'こんにちはー。岩田さん', 1);
--- chat2/src/main/resources/schema.sql
+++ chat2/src/main/resources/schema.sql
@@ -1,5 +1,13 @@
-CREATE TABLE IF NOT EXISTS employee (
-    employee_id INT PRIMARY KEY,
-    employee_name VARCHAR(50),
-    age INT
+CREATE TABLE IF NOT EXISTS user (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(64),
+    password VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS bbs (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    author VARCHAR(64),
+    subject VARCHAR(255),
+    body text,
+    delete_flg int default 0
 );
