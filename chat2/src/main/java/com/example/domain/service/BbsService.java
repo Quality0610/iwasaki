874914463a89b1bc7fbf33b5fed21c5724fdce98
WHITE-GRINT\Kazuki <kzktcy0610@gmail.com>
2020-12-31 03:56:27 +0900

    Serviceの全件取得処理

--- chat2/src/main/java/com/example/domain/service/BbsService.java
+++ chat2/src/main/java/com/example/domain/service/BbsService.java
@@ -1,5 +1,7 @@
 package com.example.domain.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,4 +27,11 @@ public class BbsService {
 		return result;
 	}
 	
+	public int count() {
+		return bbsDao.count();
+	}
+	
+	public List<Bbs> selectAllBbs(){
+		return bbsDao.selectAllBbs();
+	}
 }
