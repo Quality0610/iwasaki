46498f24beb342b6857f6171f749bfdc39059340
WHITE-GRINT\Kazuki <kzktcy0610@gmail.com>
2020-12-30 16:40:45 +0900

    Daoとその実装のImplクラス、中身は後程入れる

---
+++ chat2/src/main/java/com/example/domain/repository/BbsDao.java
@@ -0,0 +1,16 @@
+package com.example.domain.repository;
+
+import java.util.List;
+
+import org.springframework.dao.DataAccessException;
+
+import com.example.domain.model.Bbs;
+
+public interface BbsDao {
+
+	public int insertBbs(Bbs bbs) throws DataAccessException;
+	
+	public List<Bbs> selectAllBbs() throws DataAccessException;
+	
+	public int deleteBbs(int id) throws DataAccessException;
+}
---
+++ chat2/src/main/java/com/example/domain/repository/jdbc/BbsDaoJdbcImpl.java
@@ -0,0 +1,31 @@
+package com.example.domain.repository.jdbc;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.example.domain.model.Bbs;
+import com.example.domain.repository.BbsDao;
+
+public class BbsDaoJdbcImpl implements BbsDao {
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	
+	@Override
+	public int insertBbs(Bbs bbs) throws DataAccessException{
+		return 0;
+	}
+
+	@Override
+	public List<Bbs> selectAllBbs() throws DataAccessException{
+		return null;
+	}
+	
+	@Override
+	public int deleteBbs(int id) throws DataAccessException{
+		return 0;
+	}
+}
